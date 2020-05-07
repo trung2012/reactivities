8feabcd975f8a443c7f9a7c0f635024efbd20ed1
Trung Pham <trungpham2012@gmail.com>
2020-05-07 11:40:53 -0500

    added client

--- API/Startup.cs
+++ API/Startup.cs
@@ -6,27 +6,46 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Persistence;
 
-namespace API {
-    public class Startup {
-        public Startup(IConfiguration configuration) {
+namespace API 
+{
+    public class Startup 
+    {
+        public Startup(IConfiguration configuration) 
+        {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services) {
+        public void ConfigureServices(IServiceCollection services) 
+        {
+            services.AddDbContext<DataContext>(options => 
+            {
+                options.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+            });
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
-            if (env.IsDevelopment()) {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) 
+        {
+            if (env.IsDevelopment()) 
+            {
                 app.UseDeveloperExceptionPage();
             }
 
@@ -36,7 +55,10 @@ namespace API {
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints => {
+            app.UseCors("CorsPolicy");
+
+            app.UseEndpoints(endpoints => 
+            {
                 endpoints.MapControllers();
             });
         }
