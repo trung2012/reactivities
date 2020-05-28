954e5eb9df18f0cbda2de9537ce2c6dff0938e6f
Trung Pham <trungpham2012@gmail.com>
2020-05-28 11:12:11 -0500

    added photo upload capability on server

--- Application/User/Register.cs
+++ Application/User/Register.cs
@@ -73,7 +73,8 @@ namespace Application.User
                     {
                         DisplayName = user.DisplayName,
                         Token = _jwtGenerator.CreateToken(user),
-                        Username = user.UserName
+                        Username = user.UserName,
+                        Image = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
                     };
                 }
 
